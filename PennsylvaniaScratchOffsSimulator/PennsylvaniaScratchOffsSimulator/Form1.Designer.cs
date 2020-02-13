41e78ab0714bdce048ab2293e4e856cb3592d70c
Thomas Snyder <52889852+SirThominickIV@users.noreply.github.com>
2020-02-13 13:29:03 -0500

    Add files via upload

---
+++ PennsylvaniaScratchOffsSimulator/PennsylvaniaScratchOffsSimulator/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PennsylvaniaScratchOffsSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_List = new System.Windows.Forms.ComboBox();
+            this.rtb_Stats = new System.Windows.Forms.RichTextBox();
+            this.btn_Buy = new System.Windows.Forms.Button();
+            this.rtb_Output = new System.Windows.Forms.RichTextBox();
+            this.tb_Balance = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_WinLoss = new System.Windows.Forms.Label();
+            this.tb_WinLoss = new System.Windows.Forms.TextBox();
+            this.tb_TicketsBought = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menu_About = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_Help = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_Github = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cb_List
+            // 
+            this.cb_List.FormattingEnabled = true;
+            this.cb_List.Items.AddRange(new object[] {
+            "5X THE MONEY",
+            "Oh, What Fun",
+            "$3 Million Limited"});
+            this.cb_List.Location = new System.Drawing.Point(29, 51);
+            this.cb_List.Name = "cb_List";
+            this.cb_List.Size = new System.Drawing.Size(185, 21);
+            this.cb_List.TabIndex = 0;
+            this.cb_List.SelectedIndexChanged += new System.EventHandler(this.cb_List_SelectedIndexChanged);
+            // 
+            // rtb_Stats
+            // 
+            this.rtb_Stats.Location = new System.Drawing.Point(29, 88);
+            this.rtb_Stats.Name = "rtb_Stats";
+            this.rtb_Stats.Size = new System.Drawing.Size(185, 248);
+            this.rtb_Stats.TabIndex = 1;
+            this.rtb_Stats.Text = "";
+            this.rtb_Stats.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.rtb_Stats_LinkClicked);
+            // 
+            // btn_Buy
+            // 
+            this.btn_Buy.Location = new System.Drawing.Point(260, 353);
+            this.btn_Buy.Name = "btn_Buy";
+            this.btn_Buy.Size = new System.Drawing.Size(109, 23);
+            this.btn_Buy.TabIndex = 2;
+            this.btn_Buy.Text = "Buy Ticket";
+            this.btn_Buy.UseVisualStyleBackColor = true;
+            this.btn_Buy.Click += new System.EventHandler(this.btn_Buy_Click);
+            // 
+            // rtb_Output
+            // 
+            this.rtb_Output.Location = new System.Drawing.Point(220, 51);
+            this.rtb_Output.Name = "rtb_Output";
+            this.rtb_Output.Size = new System.Drawing.Size(185, 285);
+            this.rtb_Output.TabIndex = 3;
+            this.rtb_Output.Text = "";
+            this.rtb_Output.TextChanged += new System.EventHandler(this.rtb_Output_TextChanged);
+            // 
+            // tb_Balance
+            // 
+            this.tb_Balance.BackColor = System.Drawing.Color.LightGreen;
+            this.tb_Balance.Location = new System.Drawing.Point(503, 51);
+            this.tb_Balance.Name = "tb_Balance";
+            this.tb_Balance.Size = new System.Drawing.Size(96, 20);
+            this.tb_Balance.TabIndex = 4;
+            this.tb_Balance.Text = "$500";
+            this.tb_Balance.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(411, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Current Balance:";
+            // 
+            // lb_WinLoss
+            // 
+            this.lb_WinLoss.AutoSize = true;
+            this.lb_WinLoss.Location = new System.Drawing.Point(411, 88);
+            this.lb_WinLoss.Name = "lb_WinLoss";
+            this.lb_WinLoss.Size = new System.Drawing.Size(59, 13);
+            this.lb_WinLoss.TabIndex = 7;
+            this.lb_WinLoss.Text = "Total Loss:";
+            // 
+            // tb_WinLoss
+            // 
+            this.tb_WinLoss.BackColor = System.Drawing.SystemColors.Window;
+            this.tb_WinLoss.Location = new System.Drawing.Point(503, 88);
+            this.tb_WinLoss.Name = "tb_WinLoss";
+            this.tb_WinLoss.Size = new System.Drawing.Size(96, 20);
+            this.tb_WinLoss.TabIndex = 8;
+            this.tb_WinLoss.Text = "$0";
+            this.tb_WinLoss.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tb_TicketsBought
+            // 
+            this.tb_TicketsBought.Location = new System.Drawing.Point(503, 125);
+            this.tb_TicketsBought.Name = "tb_TicketsBought";
+            this.tb_TicketsBought.Size = new System.Drawing.Size(96, 20);
+            this.tb_TicketsBought.TabIndex = 9;
+            this.tb_TicketsBought.Text = "0";
+            this.tb_TicketsBought.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(411, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Tickets Bought:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu_About,
+            this.menu_Help,
+            this.menu_Github});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(628, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menu_About
+            // 
+            this.menu_About.Name = "menu_About";
+            this.menu_About.Size = new System.Drawing.Size(52, 20);
+            this.menu_About.Text = "About";
+            this.menu_About.Click += new System.EventHandler(this.menu_About_Click);
+            // 
+            // menu_Help
+            // 
+            this.menu_Help.Name = "menu_Help";
+            this.menu_Help.Size = new System.Drawing.Size(44, 20);
+            this.menu_Help.Text = "Help";
+            this.menu_Help.Click += new System.EventHandler(this.menu_Help_Click);
+            // 
+            // menu_Github
+            // 
+            this.menu_Github.Name = "menu_Github";
+            this.menu_Github.Size = new System.Drawing.Size(57, 20);
+            this.menu_Github.Text = "GitHub";
+            this.menu_Github.Click += new System.EventHandler(this.menu_Github_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 397);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_TicketsBought);
+            this.Controls.Add(this.tb_WinLoss);
+            this.Controls.Add(this.lb_WinLoss);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Balance);
+            this.Controls.Add(this.rtb_Output);
+            this.Controls.Add(this.btn_Buy);
+            this.Controls.Add(this.rtb_Stats);
+            this.Controls.Add(this.cb_List);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Pennsylvania Scratch Offs Simulator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_List;
+        private System.Windows.Forms.RichTextBox rtb_Stats;
+        private System.Windows.Forms.Button btn_Buy;
+        private System.Windows.Forms.RichTextBox rtb_Output;
+        private System.Windows.Forms.TextBox tb_Balance;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_WinLoss;
+        private System.Windows.Forms.TextBox tb_WinLoss;
+        private System.Windows.Forms.TextBox tb_TicketsBought;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menu_About;
+        private System.Windows.Forms.ToolStripMenuItem menu_Help;
+        private System.Windows.Forms.ToolStripMenuItem menu_Github;
+    }
+}
+
---
+++ PennsylvaniaScratchOffsSimulator/Source.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace PennsylvaniaScratchOffsSimulator
+{
+    public partial class Form1 : Form
+    {
+        //Variables
+        List<ScratchOff> ScratchOffs = new List<ScratchOff>();  //The List of scratch offs available
+        ScratchOff SelectedScratchOff;
+        const int StartingMoney = 500;
+        int Money = StartingMoney;
+        int TicketsBought = 0;
+
+        public Form1()
+        {            
+            AddSomeScratchData();
+            InitializeComponent();
+        }
+
+        public void AddSomeScratchData() //Data input for a couple scratch offs - Normally should be done with a .txt or .xml file, but this is just a demo
+        {
+            ScratchOff FiveTimesTheMoney = new ScratchOff("5X THE MONEY", 5, "https://www.palottery.state.pa.us/uploadedfiles/PA-1437_5X-the-Money_DATA.pdf");
+            FiveTimesTheMoney.PayoutsList.Add(new Payout(5, 9));
+            FiveTimesTheMoney.PayoutsList.Add(new Payout(10, 20));
+            FiveTimesTheMoney.PayoutsList.Add(new Payout(25, 30));
+            FiveTimesTheMoney.PayoutsList.Add(new Payout(50, 55));
+            FiveTimesTheMoney.PayoutsList.Add(new Payout(100, 333));
+            FiveTimesTheMoney.PayoutsList.Add(new Payout(250, 1600));
+            FiveTimesTheMoney.PayoutsList.Add(new Payout(500, 7500));
+            FiveTimesTheMoney.PayoutsList.Add(new Payout(1000, 12000));
+            FiveTimesTheMoney.PayoutsList.Add(new Payout(5000, 270000));
+            FiveTimesTheMoney.PayoutsList.Add(new Payout(100000, 1080000));
+            ScratchOffs.Add(FiveTimesTheMoney);
+
+            ScratchOff OhWhatFun = new ScratchOff("Oh, What Fun", 1, "https://www.palottery.state.pa.us/uploadedfiles/PA-1431_Oh-What-Fun_DATA.pdf");
+            OhWhatFun.PayoutsList.Add(new Payout(1, 8));
+            OhWhatFun.PayoutsList.Add(new Payout(2, 25));
+            OhWhatFun.PayoutsList.Add(new Payout(4, 59));
+            OhWhatFun.PayoutsList.Add(new Payout(5, 111));
+            OhWhatFun.PayoutsList.Add(new Payout(8, 250));
+            OhWhatFun.PayoutsList.Add(new Payout(10, 83));
+            OhWhatFun.PayoutsList.Add(new Payout(15, 1000));
+            OhWhatFun.PayoutsList.Add(new Payout(20, 250));
+            OhWhatFun.PayoutsList.Add(new Payout(30, 960));
+            OhWhatFun.PayoutsList.Add(new Payout(50, 1500));
+            OhWhatFun.PayoutsList.Add(new Payout(60, 12000));
+            OhWhatFun.PayoutsList.Add(new Payout(100, 6000));
+            OhWhatFun.PayoutsList.Add(new Payout(150, 120000));
+            OhWhatFun.PayoutsList.Add(new Payout(300, 690000));
+            OhWhatFun.PayoutsList.Add(new Payout(5000, 1380000));
+            ScratchOffs.Add(OhWhatFun);
+
+            ScratchOff ThreeMillionLimited = new ScratchOff("$3 Million Limited", 30, "https://www.palottery.state.pa.us/uploadedfiles/PA-1432_$3M-Limited_DATA.pdf");
+            ThreeMillionLimited.PayoutsList.Add(new Payout(30, 8));
+            ThreeMillionLimited.PayoutsList.Add(new Payout(40, 12));
+            ThreeMillionLimited.PayoutsList.Add(new Payout(50, 20));
+            ThreeMillionLimited.PayoutsList.Add(new Payout(100, 43));
+            ThreeMillionLimited.PayoutsList.Add(new Payout(150, 38));
+            ThreeMillionLimited.PayoutsList.Add(new Payout(300, 125));
+            ThreeMillionLimited.PayoutsList.Add(new Payout(500, 571));
+            ThreeMillionLimited.PayoutsList.Add(new Payout(1000, 1250));
+            ThreeMillionLimited.PayoutsList.Add(new Payout(10000, 200000));
+            ThreeMillionLimited.PayoutsList.Add(new Payout(300000, 600000));
+            ThreeMillionLimited.PayoutsList.Add(new Payout(3000000, 1200000));
+            ScratchOffs.Add(ThreeMillionLimited);
+        }
+
+        private void cb_List_SelectedIndexChanged(object sender, EventArgs e) //Updating which scratch off is selected from the drop down
+        {
+            foreach (ScratchOff scratchOff in ScratchOffs)
+            {
+                if (scratchOff.Name == cb_List.Text) SelectedScratchOff = scratchOff;
+            }
+            UpdateStatsDisplay();
+        }
+
+        public void UpdateStatsDisplay()    //Updating the display for the selected scratch off's stats
+        {
+            rtb_Stats.Clear();
+
+            try
+            {
+                rtb_Stats.Text += SelectedScratchOff.Name + System.Environment.NewLine;
+                rtb_Stats.Text += "Ticket Price: $" + SelectedScratchOff.Price + System.Environment.NewLine;
+                rtb_Stats.Text += "==========================" + System.Environment.NewLine;
+                foreach (Payout payout in SelectedScratchOff.PayoutsList)
+                {
+                    rtb_Stats.Text += "$" + String.Format("{0:n0}", payout.PayoutAmmount) + " : 1 in " + String.Format("{0:n0}", payout.Chances) + System.Environment.NewLine;
+                }
+                rtb_Stats.Text += "==========================" + System.Environment.NewLine;
+                rtb_Stats.Text += "Detailed statistics can be found here:" + System.Environment.NewLine;
+                rtb_Stats.Text += SelectedScratchOff.StatsURL;
+            }
+            catch
+            {
+                rtb_Stats.Text += "Error loading Scratch off stats..." + System.Environment.NewLine;
+            }
+        }
+
+        private void btn_Buy_Click(object sender, EventArgs e)  //Button for buying a ticket
+        {
+            if (SelectedScratchOff != null)
+            {              
+                //Checking winnings
+                int Winnings = 0;
+                var rand = new Random();
+                foreach (Payout payout in SelectedScratchOff.PayoutsList)
+                {
+                    if (rand.Next(1, payout.Chances) == 1) Winnings += payout.PayoutAmmount;
+                }
+
+                //Calculating balance change
+                Money -= SelectedScratchOff.Price;
+                Money += Winnings;
+                TicketsBought++;
+
+                //Output for overall ballance
+                tb_Balance.Text = "$" + String.Format("{0:n0}", Money);
+
+                //Main output log
+                if (Winnings == 0) rtb_Output.Text += "You lost $" + String.Format("{0:n0}", SelectedScratchOff.Price);
+                else if (Winnings == SelectedScratchOff.Price) rtb_Output.Text += "You broke even";
+                else rtb_Output.Text += "You won $" + String.Format("{0:n0}", Winnings - SelectedScratchOff.Price);
+                rtb_Output.Text += System.Environment.NewLine;
+
+                //Output for win/loss tracker
+                tb_WinLoss.Text = "$" + String.Format("{0:n0}", Math.Abs((Money - StartingMoney)));
+                if ((Money - StartingMoney) > 0)
+                {
+                    lb_WinLoss.Text = "Total Profit:";
+                    tb_WinLoss.BackColor = Color.LightGreen;
+                }
+                else
+                {
+                    lb_WinLoss.Text = "Total Loss:";
+                    tb_WinLoss.BackColor = Color.LightCoral;
+                }
+
+                //Color for current balance
+                if (Money > 0) tb_Balance.BackColor = Color.LightGreen; else tb_Balance.BackColor = Color.LightCoral;
+
+                //Tickets bought
+                tb_TicketsBought.Text = String.Format("{0:n0}", TicketsBought);
+            }
+            else rtb_Output.Text += "Error - You need to select a ticket type in the left hand corner drop down." + System.Environment.NewLine;
+        }
+
+        public class ScratchOff //The scratch offs class, for keeping track of each of the types of scratch offs
+        {
+            public string Name;    //The name of the scratch off
+            public int Price;      //The price to buy this scratch off
+            public string StatsURL; //The statistics the payouts came from
+            public List<Payout> PayoutsList = new List<Payout>();  //The list of all the possible payouts, and their prices.
+
+            public ScratchOff(string InputName, int InputPrice, string InputStatsURL)
+            {
+                Name = InputName;
+                Price = InputPrice;
+                StatsURL = InputStatsURL;
+            }
+        }
+
+        public class Payout //The possible payouts a scratch off could have
+        {
+            public int PayoutAmmount;  //The amount that gets payed if won
+            public int Chances;        //The chances of winning. For example, if this int is set to 100, then the chances of winning
+                                       //are 1 in 100.
+
+            public Payout(int InputPayoutAmmount, int InputChances)
+            {
+                PayoutAmmount = InputPayoutAmmount;
+                Chances = InputChances;
+            }
+        }
+
+        private void rtb_Output_TextChanged(object sender, EventArgs e) //Auto scrolls the output text box to the bottom
+        {
+            rtb_Output.SelectionStart = rtb_Output.Text.Length;
+            rtb_Output.ScrollToCaret();
+        }
+
+        private void menu_Help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("To use this program, left click on the drop down menu on the lefthand side and select a ticket type. Click \"Buy Ticket\" to buy a ticket. Maybe you'll win, but probably not.", "Help");
+        }
+
+        private void menu_About_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program was made to demonstrate why scratch off lotery tickets are a rip off. While it is possible to win in the short term, it is impossible to win in the long term. The only winning move is not to play.", "About");
+        }
+
+        private void menu_Github_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/SirThominickIV?tab=repositories");
+        }
+
+        private void rtb_Stats_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+    }
+}
